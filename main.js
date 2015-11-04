5393326e1f755b47784f07129c03fef458af9f50
hhernandez008 <hhernandez008@gmail.com>
2015-11-04 12:12:19 -0800

    created variables for creating dynamic board

--- main.js
+++ main.js
@@ -2,8 +2,24 @@
  * Created by Heather on 11/4/2015.
  */
 var xTurn = false;
+var gameLevel = null;
 
 $(document).ready(function(){
+    $(".modal").on("click", function(){
+
+    });
+
+    //create board (3x3 or 5x5)
+    var $container = $("<div>").addClass("col-xs-2");
+    var $animationContainer = $("<div>").append("<div>");
+    $container.append($animationContainer);
+    var $row = $("<div>").addClass("row");
+
+
+
+
+
+
     $(".tictac").on("click",".row>div",function(){
         var self = this;
         var squareName = $(self).attr("square");
