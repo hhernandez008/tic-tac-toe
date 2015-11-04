8b25c4af1aab0deda975a2c4babb31da6f926cc4
Christopher Eaton <christopherpeaton@gmail.com>
2015-11-04 12:41:31 -0800

    for loop added, needs test

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
  */
 var xTurn = false;
 
-$(document).ready(function(){
-    $(".tictac").on("click",".row>div",function(){
+$(document).ready(function () {
+    $(".center").on("click", ".row>div", function () {
         var self = this;
         var squareName = $(self).attr("square");
         console.log(squareName);
@@ -13,11 +13,27 @@ $(document).ready(function(){
 
     //turn off click handler if box filled
 
+// for loop for 3x3 game board
+    var easy = 2;
+    var hard = 4;
+    for (i = 0; i <= easy; i++) {
+        var mainChild_row = $("<div>", {
+            class: ""
+        });
+        for (j = 0; j <= easy; j++) {
+            var mainChild_div = $("<div>", {
+                class: ""
+            });
+            $(mainChild_row).append(mainChild_div);
+
+        }
+        $(".center").append(mainChild_row);
+    }
 
 
 });//end ready function
 
-function whoseTurn(self, square){
+function whoseTurn(self, square) {
     emptySquares(square);
     if (xTurn) {
         $(self).text("X");
