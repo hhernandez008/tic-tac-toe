dbc7b9f72d5339d21b6ec2b84505a97c433cd89f
hhernandez008 <hhernandez008@gmail.com>
2015-11-04 12:34:09 -0800

    array created dynamically

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
  * Created by Heather on 11/4/2015.
  */
 var xTurn = false;
-var gameLevel = null;
+var gameLevel = 3;
+var gameSquares = [];
 
 $(document).ready(function(){
     $(".modal").on("click", function(){
@@ -10,19 +11,25 @@ $(document).ready(function(){
     });
 
     //create board (3x3 or 5x5)
-    var $container = $("<div>").addClass("col-xs-2");
-    var $animationContainer = $("<div>").append("<div>");
-    $container.append($animationContainer);
-    var $row = $("<div>").addClass("row");
+    for(var i = 0; i < gameLevel; i++){
+        gameSquares[i] = [];
+        var $row = $("<div>").addClass("row");
+        $(".center").append($row);
+        for(var j = 0; j < gameLevel; j++){
+            var $container = $("<div>").addClass("col-xs-2").attr("id",j);
+            var $animationContainer = $("<div>").append("<div>");
+            $container.append($animationContainer);
+            $($row).append($container);
+            gameSquares[i][j] = $container;
+        }
+    }
 
 
 
 
-
-
-    $(".tictac").on("click",".row>div",function(){
+    $(".center").on("click",".row>div",function(){
         var self = this;
-        var squareName = $(self).attr("square");
+        var squareName = $(self);
         console.log(squareName);
         whoseTurn(self, squareName);
     });
@@ -34,7 +41,6 @@ $(document).ready(function(){
 });//end ready function
 
 function whoseTurn(self, square){
-    emptySquares(square);
     if (xTurn) {
         $(self).text("X");
         xTurn = false;
