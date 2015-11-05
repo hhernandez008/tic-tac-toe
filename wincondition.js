041c5fd837cd3a23e7d6b5b8b974fa1f4a0a5880
David Goodman <deg5112@gmail.com>
2015-11-04 18:10:46 -0800

    update

--- wincondition.js
+++ wincondition.js
@@ -1,8 +1,8 @@
 function win(n){
     var arr = [
-        [0, 1, 0],
-        [1, 0, 0],
-        [0, 0, 1]
+        [1, 1, 0],
+        [1, 0, 1],
+        [0, 1, 0]
     ];
     var count = 0;
     var i = 0;
@@ -19,48 +19,26 @@ function win(n){
             count = 0;
         }
     }
-    //check vertical index 0
-
-    for(var y = 0; y<arr.length;y++){
-        if(arr[y][i]===n){
-            console.log(arr[y][i] );
-            count+=1;
+    //check vertical 
+    while(i<arr.length){
+        for(var y = 0; y<arr.length;y++){
+            if(arr[y][i]===n){
+                count+=1;
+            }
+        }
+        if(count===arr.length){
+            return 'win';
+        }else{
+            count = 0;
+            i+=1;
         }
     }
-    if(count===arr.length){
-        return 'win';
-    }else{
-        count = 0;
-        i+=1;
-    }
-    //check vertical index 1
 
-    for(var y = 0; y<arr.length;y++){
-        if(arr[y][i]===n){
-            count+=1;
-        }
-    }
-    if(count===arr.length){
-        return 'win';
-    }else{
-        count = 0;
-        i+=1;
-    }
-
-    //check vertial index2
-    for(var y = 0; y<arr.length;y++){
-        if(arr[y][i]===n){
-            count+=1;
-        }
-    }
-    if(count===arr.length){
-        return 'win';
-    }else{
-        count = 0;
-        i=0;
-    }
+    i=0;
     //check diagnol left to right
     for(var z = 0; z<arr.length;z++){
+        console.log('i  ' + i);
+        console.log(arr[z][i]);
         if(arr[z][i]===n){
             count+=1;
         }
@@ -70,7 +48,7 @@ function win(n){
         return 'win';
     }else{
         count = 0;
-        i=2; //make i=2 so we go backwards on the next check
+        i=arr.length-1; //make i=2 so we go backwards on the next check
     }
     //check diagnol right to left
     for(var k = 0; k<arr.length;k++){
@@ -85,5 +63,8 @@ function win(n){
         count = 0;
         i=0;
     }
+
 }
+
+win(0);
 
