1c7225a233d2c9eb65fb5630c0e98f0e45ec361d
David Goodman <deg5112@gmail.com>
2015-11-17 16:32:55 -0800

    done with gun fix and centering, along with x and os

--- gun.js
+++ gun.js
@@ -6,13 +6,15 @@ function mouse(e) {
     var center_y = (offset.top) + ( $('#pointer-wrap').height() / 2 );
     var radians = Math.atan2(e.pageX - center_x, e.pageY- center_y);
     var degree = (radians * (180 / Math.PI) * -1) + 180;
+    var viewPortWidth = document.documentElement.clientWidth;
+    var half = viewPortWidth/2;
+    console.log(viewPortWidth);
 
-    if(e.clientX<1200){
-        pw.css('-webkit-transform', 'rotate3d(-1.5,0, 1,'+degree+'deg)');
+    if(e.clientX>half){
+        pw.css('-webkit-transform', 'rotate3d(-2,0, 1,'+degree+'deg)');
     }else{
-        pw.css('-webkit-transform', 'rotate3d(1.5,0, 1,'+degree+'deg)');
+        pw.css('-webkit-transform', 'rotate3d(2 ,0, 1,'+degree+'deg)');
     }
-
 
 }
 
